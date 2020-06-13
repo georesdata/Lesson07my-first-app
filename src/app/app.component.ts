227ec2bcb584c1396588c5f1f0dc344fa781bfbb
georesdata <grdobbs@gmail.com>
2020-06-13 16:31:58 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'The "I have changed the title"';
 }
